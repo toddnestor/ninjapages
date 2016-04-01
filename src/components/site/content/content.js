8c803101e382e97b7ae4f6bb0cbdd94ddeb15a76
Todd Nestor <todd.nestor@gmail.com>
2016-03-31 22:12:02 -0700

    now we can display content publicly!

---
+++ src/components/site/content/content.js
@@ -0,0 +1,24 @@
+var app = angular.module("app");
+
+app.config(function($stateProvider){
+	$stateProvider
+		.state("site.content",{
+			url: "/:permalink",
+			data: { pageTitle: 'Site Content ' },
+			templateUrl: "/templates/site/content/content.html",
+			controller: "SiteContentController",
+            resolve: {
+                content: function( $stateParams, Herc ) {
+                    return Herc.one('Content/getByPermalink', $stateParams.permalink).get();
+                }
+            }
+		})
+}); 
+
+app.controller("SiteContentController", function ($scope, $rootScope, $state, $stateParams, Herc, content) {
+	$scope.content = content;
+
+    $scope.getContent = function() {
+        return $scope.content;
+    }
+});
--- src/components/site/templates/standard/standard.js
+++ src/components/site/templates/standard/standard.js
@@ -5,28 +5,10 @@ app.config(function($stateProvider){
 		.state("site.templates.standard",{
 			url: "/standard",
 			data: { pageTitle: 'Site Templates Standard ' },
-			templateUrl: "/templates/site/templates/standard/standard.html",
-			controller: "SiteTemplatesStandardController"
+			templateUrl: "/templates/site/templates/standard/standard.html"
 		})
 }); 
 
 app.controller("SiteTemplatesStandardController", function ($scope, $rootScope, $state, $stateParams, Herc) {
-	$scope.sections = [
-        'header',
-        'leftImage',
-        'logoStrip',
-        'codeSample',
-        'quote',
-        'codeSample',
-        'logoStrip',
-        'codeSample',
-        'features',
-        'logoStrip',
-        'features',
-        'header',
-        'pricing',
-        'logoStrip',
-        'pricing',
-        'footer'
-    ];
+    $scope.content = $scope.getContent();
 });
