8912d8fe84ac65b7256570df75824b21a508409e
Todd Nestor <todd.nestor@gmail.com>
2016-05-12 23:36:24 -0700

    changed template back to standard

--- src/components/builder/builder.js
+++ src/components/builder/builder.js
@@ -473,7 +473,7 @@ app.controller("BuilderController", function ($scope, $rootScope, $state, $state
             ]
         };
 
-    $scope.currentTemplate = 'corporate';
+    $scope.currentTemplate = 'standard';
 
     $scope.currentSectionOptions = [];
 
