dd53eee73872ef5e663c8acc2dac9cc910e96bab
Todd Nestor <todd.nestor@gmail.com>
2016-12-04 10:56:16 -0800

    projects and categories can be added to portfolio

--- src/app/directives/isotope.js
+++ src/app/directives/isotope.js
@@ -1,35 +1,36 @@
 var app = angular.module( "app" );
 
-app.directive('isotope', function($parse) {
+app.directive('isotope', function($parse, $timeout) {
     return {
-        link: function(scope, element, attr) {
-          var $container = $(element).find('.portfolioContainer');
+        link: function(scope, element, attr, ngModel) {
+          $timeout(function(){
+            var $container = $(element).find('.portfolioContainer');
 
-          $container.isotope({
-  						filter: '*',
-  						animationOptions: {
-  								duration: 750,
-  								easing: 'linear',
-  								queue: false
-  						}
-  				});
+            $container.isotope({
+              filter: '*',
+              animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+              }
+            });
 
-          $(element).on('click', 'a[data-filter]', function(){
-  						$('.portfolioFilter .current').removeClass('current');
-  						$(this).addClass('current');
-              console.log('what we filtering?', $(this).attr('data-filter') );
+            $(element).on('click', 'a[data-filter]', function(){
+              $('.portfolioFilter .current').removeClass('current');
+              $(this).addClass('current');
 
-  						var selector = $(this).attr('data-filter');
-  						$container.isotope({
-  								filter: selector,
-  								animationOptions: {
-  										duration: 750,
-  										easing: 'linear',
-  										queue: false
-  								}
-  						});
-  						return false;
-  				})
+              var selector = $(this).attr('data-filter');
+              $container.isotope({
+                filter: selector,
+                animationOptions: {
+                  duration: 750,
+                  easing: 'linear',
+                  queue: false
+                }
+              });
+              return false;
+            });
+          }, 500);
         }
     }
 } );
--- src/components/site/templates/portfolio1/section/projects/projects.js
+++ src/components/site/templates/portfolio1/section/projects/projects.js
@@ -1,5 +1,89 @@
-var app = angular.module("app"); 
+var app = angular.module("app");
 
 app.controller("SiteTemplatesPortfolio1SectionProjectsController", function ($scope, $rootScope, $state, $stateParams, Herc) {
-	//All your code goes here
+	$scope.section.options = $scope.section.options || {
+			settings: [
+
+			]
+	};
+
+	$scope.section.settings = $scope.section.settings || {
+		section_background_color: '#ffffff',
+		section_background_image: '',
+		categories: [
+			'Design',
+			'Development',
+			'Branding',
+			'Ruby',
+			'Angular',
+			'Rails'
+		],
+		projects: [
+			{
+				name: 'Digital Pen',
+				url: '/whatever',
+				image_url: 'https://herc.objects.cdn.dream.io/uploads/a05a5b067e8a99376bfd73d3b228160a/bg1.jpg',
+				categories: [
+					'Design',
+					'Branding'
+				]
+			},
+			{
+				name: 'Digital Pen',
+				url: '/whatever',
+				image_url: 'https://herc.objects.cdn.dream.io/uploads/a05a5b067e8a99376bfd73d3b228160a/bg1.jpg',
+				categories: [
+					'Design',
+					'Branding'
+				]
+			},
+			{
+				name: 'Digital Pen',
+				url: '/whatever',
+				image_url: 'https://herc.objects.cdn.dream.io/uploads/a05a5b067e8a99376bfd73d3b228160a/bg1.jpg',
+				categories: [
+					'Design',
+					'Branding'
+				]
+			},
+			{
+				name: 'Digital Pen',
+				url: '/whatever',
+				image_url: 'https://herc.objects.cdn.dream.io/uploads/a05a5b067e8a99376bfd73d3b228160a/bg1.jpg',
+				categories: [
+					'Design',
+					'Branding'
+				]
+			}
+		]
+	};
+
+	$scope.classify = function(category) {
+		return category.replace(/[^a-z0-9\-\_]/gi,'');
+	}
+
+	$scope.classes = function(categories) {
+		return categories.map(function(category) { return $scope.classify(category)}).join(' ');
+	}
+
+	$scope.addCategory = function() {
+		$scope.section.settings.categories.push('New Category');
+	}
+
+	$scope.addProject = function() {
+		console.log('we got called at least...');
+		$scope.section.settings.projects.push({
+			name: 'New Project',
+			url: '/whatever',
+			image_url: 'https://herc.objects.cdn.dream.io/uploads/a05a5b067e8a99376bfd73d3b228160a/bg1.jpg',
+			categories: [
+				'Design',
+				'Branding'
+			]
+		});
+	}
+
+	$scope.editCategory = function() {
+
+	}
 });
